9b992277571c22fbf838e65c0cffe136a0ad04d4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-02 22:52:28 +0600

    add

--- src/helpers/regex.rs
+++ src/helpers/regex.rs
@@ -26,10 +26,12 @@ pub fn get_content_between_first_brackets(text: &str) -> Result<String, String> 
     if let Some(captures) = pattern.captures(text) {
         if let Some(inner_text) = captures.get(1) {
             return Ok(inner_text.as_str().trim().to_string())
+        }else{
+            return Err("No Text found".to_string());
         }
     }
     
-    return Err("No Text found".to_string());
+    return Err("No capture found".to_string());
 }
 
 #[cfg(test)]
@@ -39,13 +41,13 @@ mod tests {
     #[test]
     fn test_get_id_from_url_perfect_regex() {
         let id = get_id_from_url("https://myanimelist.net/anime/12189/");
-        assert_eq!(id.unwrap(), 12189);
+        assert_eq!(id.to_owned().unwrap(), 12189);
     }
     
     #[test]
     fn test_get_id_from_url_bad_regex(){
         let id = get_id_from_url("https://myanimelist.net/anime/38101/5-toubun_no_Hanayome");
-        assert_eq!(id.
+        assert_eq!(id.to_owned().
             unwrap(), 38101);
     }
 
@@ -53,14 +55,14 @@ mod tests {
     fn test_get_id_from_url_no_match(){
         let id  = get_id_from_url("https://myanimelist.net/anime");
         assert!(id.is_err());
-        assert_eq!(id.unwrap_err(), "No capture found");
+        assert_eq!(id.to_owned().unwrap_err(), "No capture found");
     }
 
     #[test]
     fn test_capture_between_first_brackets(){
         let text = get_content_between_first_brackets("Sora Amamiya ( 雨宮 天 )");
-        assert_ne!(text.unwrap(), " 雨宮 天 ");
-        assert_eq!(text.unwrap(), "雨宮 天");
+        assert_ne!(text.to_owned().unwrap(), " 雨宮 天 ");
+        assert_eq!(text.to_owned().unwrap(), "雨宮 天");
 
     }
 
@@ -68,8 +70,8 @@ mod tests {
     fn test_capture_between_first_brackets_no_match(){
         let text = get_content_between_first_brackets("Sora Amamiya");
 
-        assert!(text.is_err());
-        assert_eq!(text.unwrap_err(), "No capture found");
+        assert!(text.to_owned().is_err());
+        assert_eq!(text.to_owned().unwrap_err(), "No capture found");
     }
 
 
