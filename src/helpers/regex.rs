3c0415f57558b11e970ba0ba64b6c61a103e5ccf
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-02 22:29:14 +0600

    Update regex.rs

--- src/helpers/regex.rs
+++ src/helpers/regex.rs
@@ -24,7 +24,7 @@ pub fn get_id_from_url(url: &str) -> Result<i64, String> {
 pub fn get_content_between_first_brackets(text: &str) -> Result<i64, String> {
     let pattern = Regex::new(r"\((.*?)\)").unwrap();
     if let Some(captures) = pattern.captures(text) {
-        if let Some(text) = captures.get(1) {
+        if let Some(inner_text) = captures.get(1) {
             return Ok(inner_text.as_str().to_string())
         }else{
             return Err("No Text found".to_string());
