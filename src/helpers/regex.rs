8446bbc3fdbec0ab3b72145ed6e8d955c49ed55c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-02 22:29:26 +0600

    Update regex.rs

--- src/helpers/regex.rs
+++ src/helpers/regex.rs
@@ -61,8 +61,8 @@ mod tests {
 
     #[test]
     fn test_capture_between_first_brackets(){
-        let text = get_content_between_first_brackets("Sora Amamiya ( 雨宮 天 )")
-        assert_eq!(text.unwrap()," 雨宮 天 ")
+        let text = get_content_between_first_brackets("Sora Amamiya ( 雨宮 天 )");
+        assert_eq!(text.unwrap()," 雨宮 天 ");
     }
 
 }
